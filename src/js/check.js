0882106a7f7998b05261e4904f7bb9c5c7632717
Kornienko Denis <korney197823@gmail.com>
2016-10-05 21:04:14 +0300

    Fixed the quotes in the same style, removed unnecessary else statement and loops

--- src/js/check.js
+++ src/js/check.js
@@ -1,31 +1,35 @@
+'use strict';
+
 function getMessage(a, b) {
-  if(typeof a === "boolean") {
+  if(typeof a === 'boolean') {
     if (a) {
       return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
     }
     else {
       return 'Переданное GIF-изображение не анимировано';
     }
-  } else if (typeof a === "number") {
+  }
+  if (typeof a === 'number') {
     return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + (b * 4) +  ' атрибутов';
-  }  else if (Array.isArray(a) && Array.isArray(b)) {
-    if (a.length === b.length) {
-      var multiplyElementArray = [];
-      var artifactsSquare = 0;
-      for(var j = 0; j < a.length; j++) {
-        multiplyElementArray.push(a[j] * b[j]);
-      }
-      for(var k = 0; k < multiplyElementArray.length; k++) {
-        artifactsSquare += multiplyElementArray[k];
-      }
+  }
+  if (Array.isArray(a) && Array.isArray(b)) {
+    var multiplyElementArray = [];
+    var artifactsSquare = 0;
+    for(var j = 0; j < a.length; j++) {
+      multiplyElementArray.push(a[j] * b[j]);
+      artifactsSquare += multiplyElementArray[j];
     }
     return 'Общая площадь артефактов сжатия: ' + artifactsSquare + ' пикселей';
-  } else if (Array.isArray(a)) {
+  }
+  if (Array.isArray(a) && !Array.isArray(b)) {
     var amountOfRedPoints = 0;
     for (var i = 0; i < a.length; i++) {
       amountOfRedPoints  += a[i];
     }
     return 'Количество красных точек во всех строчках изображения: ' + amountOfRedPoints;
+  }
+  else {
+    return 'Переданы некорректные данные';
   }
 
 }
